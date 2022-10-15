97bd0384aae8aed98ec138f11aa8c046efc25f8d
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-15 01:00:27 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -15,3 +15,10 @@ cnum = len(numbers[0])
 # ******************************
 # Make your Code
 # ******************************
+crosses = 0
+for i in range(1,cnum - 1, 1):
+    for j in range(1,rnum-1,1):
+        if(numbers[j][i] == 1 and numbers[j][i - 1] == 1 and numbers[j][i + 1] == 1 and numbers[j-1][i] == 1 and numbers[j+1][i] == 1):
+            crosses += 1
+    
+print(crosses)
